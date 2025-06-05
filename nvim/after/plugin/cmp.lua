47ed21a6db37e13583bea2f445a10351ff7530b1
RuS2m <rustam.guseynzade@gmail.com>
2025-06-05 20:27:25 +0100

    feat(mappings): autocomplete suggestions listing on tab/Stab rather than arrows

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -12,6 +12,8 @@ cmp.setup({
         ['<S-j>'] = cmp.mapping.scroll_docs(4),
         ['<S-Space>'] = cmp.mapping.complete(),
         ['<S-e>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
