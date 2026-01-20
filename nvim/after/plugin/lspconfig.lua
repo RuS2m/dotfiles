ad2ac7e9de673e6f3a32c149abe522d1a9f90581
Rustam Gusein-zade <rustam.guseynzade@gmail.com>
2026-01-20 18:02:16 +0000

    feat(clangd): add swith from header to source function

--- nvim/after/plugin/lspconfig.lua
+++ nvim/after/plugin/lspconfig.lua
@@ -1,6 +1,28 @@
 -- lsp server settings
 local nvim_lsp = require('lspconfig')
 
+-- clang-specific magic, due to work with header files
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr, client)
+  local method_name = 'textDocument/switchSourceHeader'
+  ---@diagnostic disable-next-line:param-type-mismatch
+  if not client or not client:supports_method(method_name) then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  ---@diagnostic disable-next-line:param-type-mismatch
+  client:request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
 -- unified lsp mappings
 local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
@@ -35,6 +57,9 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'wf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
   vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.diagnostic.open_float(nil, { focusable = false })')
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-y>', '<cmd>lua vim.diagnostic.open_float(nil, { focusable = false })<CR>', opts)
+  vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+      switch_source_header(bufnr, client)
+    end, { desc = 'Switch between source/header' })
 end
 
 vim.lsp.config('rust_analyzer', {
